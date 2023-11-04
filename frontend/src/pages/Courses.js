43ed33e60c9902429372dc20ca6a978be4cd3509
elee201 <leee73@mymacewan.ca>
2023-11-03 20:11:15 -0600

    fixed sign-in flow,review-UI

--- frontend/src/pages/Courses.js
+++ frontend/src/pages/Courses.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css'; 
 import { useNavigate } from 'react-router-dom';
+import Popup from './popup';
+import CustomSelect from './CustomSelect';
 
 function Courses() {
 
 	const navigate = useNavigate();
 
+	const[isOpen, setIsOpen] = useState(false);
+
+	const togglePopup =() =>{
+		setIsOpen(!isOpen);
+	}
+
+	const [reviewData, setReviewData] = useState({
+		Professor: "",
+		Grade: '',
+		reviewText: '',
+		Difficulty: '',
+		Workload: '',
+	}); // State to store the review text
+
+	const handleAddReview = () => {
+	 // Access the review data
+	const { Difficulty,Workload,Professor, Grade, reviewText } = reviewData;
+
+	 // You can handle the review submission here
+	 // For now, just log the review data
+	console.log('Professor:', Professor);
+	console.log('Grade:', Grade);
+	console.log('Review Text:', reviewText);
+	console.log("Difficulty",Difficulty);
+	console.log("Workload",Workload);
+	};
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setReviewData({ ...reviewData, [name]: value });
+	};
+
+	function onChangeInput(value){
+		console.log(value);
+	}
+	
 	return( 
 		<div className='courses-container'> 
 			<header className="black-bar" > 
@@ -52,6 +90,66 @@ function Courses() {
 					<div className="misc-tag">Weekly quizzes</div>
 					<div className="misc-tag">Weekly quizzes</div>
 				</div>
+			</div>
+
+			<div className="popup-review" >
+			<button className="add-review-btn" onClick={togglePopup}>Add a review
+
+			</button>
+				{isOpen && <Popup
+				handleClose={togglePopup}
+				content={<div>
+					<h2>Add a review</h2>
+
+
+					<input 
+						type='text'
+						name="Workload"
+						value={reviewData.Workload}
+						onChange={handleInputChange}
+						placeholder='Workload'
+					/>
+
+					<input 
+						type='text'
+						name="Difficulty"
+						value={reviewData.Difficulty}
+						onChange={handleInputChange}
+						placeholder='Difficulty'
+					/>
+
+
+					<input 
+						type='text'
+						name="Professor"
+						value={reviewData.Professor}
+						onChange={handleInputChange}
+						placeholder='Professor'
+					/>
+					
+					<input 
+						type='text'
+						name="Grade"
+						value={reviewData.Grade}
+						onChange={handleInputChange}
+						placeholder='Grade'
+					/>
+					<textarea
+					rows="4"
+					cols="50"
+					name="reviewText"
+					value={reviewData.reviewText}
+					onChange={handleInputChange}
+					placeholder='Write your review here'
+					/>
+					<CustomSelect isMulti={true} onChange={onChangeInput} />
+					
+					<button onClick={handleAddReview}>Submit Review</button>
+
+					
+					</div>}
+				/>}
+			
 			</div>
 
 			<div className="bottom-black-bar">
