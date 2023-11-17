1985f4670b7ebbae57974c4403c413cfc19e908d
elee201 <leee73@mymacewan.ca>
2023-11-17 15:09:46 -0700

    created a navbar

---
+++ frontend/src/pages/navbar.js
@@ -0,0 +1,56 @@
+// Navbar.js
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from React Router
+import SearchResults from './SearchResult';
+import { useUser } from '../UserContext'; // Import the useUser hook
+
+
+function Navbar() {
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  const { user, handleLogout } = useUser(); // Access handleLogout from the context
+
+  const handleLogoClick = () => {
+    // Navigate to the landing page when the logo is clicked
+    navigate('/');
+  };
+
+  const handleViewProfileClick = () => {
+    navigate('/ViewProfile')
+};
+
+const handleLoginClick = () => {
+  navigate('/Login'); // Navigate to the sign-in page
+};
+
+
+  return (
+    <div className='navbar'>
+      <div className='logo-navbar' onClick={handleLogoClick}>
+      <img src='/Course_rev_logo_nav.png' alt='' className='logo-navbar'></img>
+      </div>
+
+      <div className='search-bar-navbar'>
+        <SearchResults />
+      </div>
+      <div className='top-right-container'>
+                    {Object.keys(user).length !== 0 ? (
+                    <div>
+                        {/*if logged in*/}
+                        <button className="logout-button" onClick={handleLogout}>Logout</button>
+                        
+                        <button className='ViewProfile-user-btn' onClick={handleViewProfileClick}>hey {user.name}</button>
+                        
+                    </div>
+                    ) : (
+                        
+                    <button className="login-button" onClick={handleLoginClick}>Login</button>
+                    )}
+                    
+                    </div>
+    </div>
+  );
+}
+
+export default Navbar;
